a461e460a1e280698365ac4118b8a9d45ca288b0
vuphuong1794 <phuongvu1794@gmail.com>
2025-06-28 21:50:49 +0700

    try deploy

--- src/common/firebase-admin.ts
+++ src/common/firebase-admin.ts
@@ -5,13 +5,13 @@ export function initializeFirebase() {
     if (admin.apps.length === 0) {
         let serviceAccount;
 
-        if (process.env.isProd === 'true') {
+        if (process.env.isProd === "1") {
             // Trên production (Render), sử dụng environment variables
             serviceAccount = {
                 type: "service_account",
                 project_id: process.env.FIREBASE_PROJECT_ID,
                 private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-                private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+                private_key: process.env.FIREBASE_PRIVATE_KEY,
                 client_email: process.env.FIREBASE_CLIENT_EMAIL,
                 client_id: process.env.FIREBASE_CLIENT_ID,
                 auth_uri: "https://accounts.google.com/o/oauth2/auth",
